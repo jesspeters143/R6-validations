bdb0c9f1201256c195441a72f7aa49e73fa4c4c8
Jessica <jesspeters143@gmail.com>
2022-01-19 18:32:04 -0800

    first commit

---
+++ app/models/order.rb
@@ -0,0 +1,5 @@
+class Order < ApplicationRecord
+    belongs_to :customer
+    validates :product_name, presence: true 
+    validates :product_count, presence: true
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'customers#index'
   resources :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :customers do
+    resources :orders
+  end
 end
