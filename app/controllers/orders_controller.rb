d5e62aa3969e3dde8a186985e4808f57f35ba425
Jessica <jesspeters143@gmail.com>
2022-01-20 15:21:24 -0800

    worked on some changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,17 @@
 class OrdersController < ApplicationController
-    def create
-        @customer = Customer.find(params[:customer_id])
-        @order = @customer.orders.create(order_params)
-        redirect_to customer_path(@customer)
-      end
+  def new
+    @order = Orders.new
+  end
+
+  def create
+    @customer = Customer.find(params[:customer_id])
+    @order = @customer.orders.create(order_params)
+    redirect_to customer_path(@customer)
+  end
     
-      private
-        def order_params
-          params.require(:order).permit(:product_name, :product_count)
-        end
- end
+  private
+    def order_params
+      params.require(:order).permit(:product_name, :product_count)
+    end
+  end
  
