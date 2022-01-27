564fc1f1a5ceb725a4aacb58c103a86ce3d6176d
Jessica <jesspeters143@gmail.com>
2022-01-26 21:10:12 -0800

    added some styling to the page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,74 @@
 class OrdersController < ApplicationController
-  def new
-    @order = Orders.new
+  rescue_from ActiveRecord::RecordNotFound, with: :catch_not_found
+
+  #layout 'order_layout'
+  # GET /orders or /orders.json
+  def index
+    @orders = Order.all
   end
 
-  def create
-    @customer = Customer.find(params[:customer_id])
-    @order = @customer.orders.create(order_params)
-    redirect_to customer_path(@customer)
+  # GET /orders/1 or /orders/1.json
+  def show
+    @order = Order.find(params[:id])
   end
-    
-  private
-    def order_params
-      params.require(:order).permit(:product_name, :product_count)
+
+  # GET /orders/new
+  def new
+    @order = Order.new
+  end
+
+  # GET /orders/1/edit
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  # POST /orders or /orders.json
+  def create
+    @order = Order.new(order_params)
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to orders_path, notice: 'Order was successfully created.' }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
     end
   end
- 
+
+
+  # PATCH/PUT /orders/1 or /orders/1.json
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      flash.notice = "The order record was updated"
+      redirect_to @order
+    else
+      flash.now.alert = @order.errors.full_messages.to_sentence
+      render :edit
+    end
+  end
+
+
+  # DELETE /orders/1 or /orders/1.json
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    respond_to do |format|
+      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through
+    def order_params
+      params.require(:order).permit(:product_name, :product_count, :customer_id)
+    end
+
+    def catch_not_found(e)
+      Rails.logger.debug("We had a 'not found' exception")
+          flash.alert = e.to_s
+          redirect_to customers_path
+    end
+end
